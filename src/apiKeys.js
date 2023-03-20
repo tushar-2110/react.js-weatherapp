ed840653830186904e92f87fac160d49a4314a01
Tushar <tusharsharma123456.k20@gmail.com>
2023-03-20 20:43:17 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c2a8da9cdfcd0a50b0135119894f365c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
